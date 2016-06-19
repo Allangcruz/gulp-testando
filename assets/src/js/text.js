74fe91a92d6e587c6c364c81033e2262dbbf1b76
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2016-06-19 19:35:42 -0300

    Integrando gulp-uglify

---
+++ assets/src/js/text.js
@@ -0,0 +1,5 @@
+var app = (function (document) {
+	document.getElementById('logo').innerHTML = "Gulp";
+	document.getElementById('logo-desc').innerHTML = "Automate and enhance your workflow";
+	document.getElementById('video').innerHTML = "Gulp uglify";
+})(document);
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log("Bom dia!");
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+gulp.task('default', ['sass', 'js']);
+
+gulp.task('sass', function () {
+	return gulp.src('assets/src/sass/**/*.scss')
+		.pipe(concat('style.min.js'))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('assets/src/js/**/*.js')
+	.pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'));
 });
