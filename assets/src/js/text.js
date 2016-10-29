1dbc58bfd93cac70e4dbb11787a27a379325ad3e
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2016-10-29 19:05:30 -0200

    Testando Gulp

--- assets/src/js/text.js
+++ assets/src/js/text.js
@@ -2,4 +2,6 @@ var app = (function (document) {
 	document.getElementById('logo').innerHTML = "Gulp";
 	document.getElementById('logo-desc').innerHTML = "Automate and enhance your workflow";
 	document.getElementById('video').innerHTML = "Gulp uglify";
+	console.log('erra uma vez uma menina chamada Love of my Life');
+	console.log('Allan 123');
 })(document);
--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	htmlmin = require('gulp-htmlmin');
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('default', ['sass', 'js', 'image-min', 'html-min', 'watch']);
 
 gulp.task('sass', function () {
 	return gulp.src('assets/src/sass/**/*.scss')
@@ -20,3 +22,25 @@ gulp.task('js', function() {
     .pipe(uglify())
     .pipe(gulp.dest('assets/js'));
 });
+
+gulp.task('image-min', function() {
+  return gulp.src('assets/src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('assets/img'));
+});
+
+gulp.task('html-min', function() {
+  return gulp.src('_html/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('.'));
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('assets/src/sass/**/*.scss', ['sass']);
+	gulp.watch('assets/src/js/**/*.js', ['js']);
+	gulp.watch('assets/src/img/*', ['image-min']);
+	gulp.watch('_html/**/*.html', ['html-min']);
+});
+
+
